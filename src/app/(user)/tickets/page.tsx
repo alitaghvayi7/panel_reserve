ef438599b0c27352146036e8150bc7d2411abc05
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2023-12-29 19:40:16 +0330

    fix error

--- src/app/(user)/tickets/page.tsx
+++ src/app/(user)/tickets/page.tsx
@@ -2,12 +2,10 @@ import TicketsPageMainLayout from "@/components/TicketsPage/MainLayout";
 
 const TicketsPage = async ({
   searchParams,
-  children,
 }: {
   searchParams: {
     [key: string]: string;
   };
-  children?: React.ReactNode;
 }) => {
   // const session = await getServerSession(nextAuthOptions);
 
